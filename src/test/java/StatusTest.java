342cc9be586d39828156e3c7175e175c4ab59f38
jordanrobbedthesun <jordanrobbedthesun@gmail.com>
2025-04-07 13:10:26 -0400

    Fixed structure of status class

--- src/test/java/StatusTest.java
+++ src/test/java/StatusTest.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.openqa.selenium.WebElement;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -18,15 +17,19 @@ public class StatusTest {
     WebDriver driver;
     WebDriverWait wait;
     Actions action;
+    Robot robot;
+
     @BeforeClass
-    public void setUp() {
+    public void setUp() throws AWTException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\jorda\\OneDrive - Florida Gulf Coast University\\School Years\\Junior Year\\Spring 2025\\Software Testing\\drivers\\geckodriver.exe");
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         action = new Actions(driver);
+        robot = new Robot();
     }
+
     @Test(priority = 1)
-    public void testStatus() throws InterruptedException, AWTException {
+    public void testTextStatus() throws InterruptedException {
         // Get website
         driver.get("https://web.whatsapp.com/");
 
@@ -36,15 +39,12 @@ public class StatusTest {
         Thread.sleep(2000);
 
 
-
-
         // Click status
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/header/div/div/div/div/span/div/div[1]/div[2]/button/span")).click();
         Thread.sleep(2000);
 
         // Add text status and change settings
-        WebElement Status_Add_Check = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/div/div/div[1]/div"));
-        Status_Add_Check.click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/div/div/div[1]/div")).click();
         Thread.sleep(2000);
 
         // Click texts
@@ -77,13 +77,10 @@ public class StatusTest {
         // Send status
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[3]/div/div/div[3]/div/div[2]/div")).click();
         Thread.sleep(2000);
+    }
 
-
-
-
-
-
-
+    @Test(priority = 2)
+    public void testImageStatus() throws InterruptedException {
         // Add image status and change settings
         // Click add
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/div/header/div/span/div/div[1]/button")).click();
@@ -102,7 +99,6 @@ public class StatusTest {
         Thread.sleep(2000);
 
         // Robot to close the file explorer because it hates me
-        Robot robot = new Robot();
         robot.keyPress(KeyEvent.VK_ESCAPE);
         robot.keyRelease(KeyEvent.VK_ESCAPE);
         Thread.sleep(2000);
@@ -117,16 +113,12 @@ public class StatusTest {
         // Send status
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[3]/div/div/div/div/div[2]/div[2]/div/div")).click();
         Thread.sleep(2000);
+    }
 
-
-
-
-
-
-
-
+    @Test(priority = 3)
+    public void testViewStatus() throws InterruptedException {
         // View statuses
-        Status_Add_Check.click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/div/div/div[1]/div")).click();
         Thread.sleep(1000);
 
         // Click views
@@ -155,19 +147,16 @@ public class StatusTest {
         // Exit early
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[3]/div/div/div/button[1]/span")).click();
         Thread.sleep(2000);
+    }
 
-
-
-
-
-
-
+    @Test(priority = 4)
+    public void testDeleteStatus() throws InterruptedException {
         // Delete text Status
-        Status_Add_Check.click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/div/div/div[1]/div")).click();
         Thread.sleep(1000);
 
         // Delete status and confirm
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[3]/div/div/div/span/div/div/div/div[2]/div[3]/div/div/div[2]/span/div/button")).click();;
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[3]/div/div/div/span/div/div/div/div[2]/div[3]/div/div/div[2]/span/div/button")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[5]/div/ul/div/li")).click();
         Thread.sleep(2000);
@@ -175,25 +164,20 @@ public class StatusTest {
         Thread.sleep(2000);
 
         // Delete image status
-        Status_Add_Check.click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/div/div/div[1]/div")).click();
         Thread.sleep(2000);
 
         // Delete status and confirm
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[3]/div/div/div/span/div/div/div/div[2]/div[3]/div/div/div[2]/span/div/button")).click();;
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[3]/div/div/div/span/div/div/div/div[2]/div[3]/div/div/div[2]/span/div/button")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[5]/div/ul/div/li[2]")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/span[2]/div/div/div/div/div/div/div[2]/div/button[2]")).click();
         Thread.sleep(2000);
+    }
 
-
-
-
-
-
-
-
-
+    @Test(priority = 5)
+    public void testChangePrivacy() throws InterruptedException {
         // Change status privacy
         // Click 3 dots
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/div/header/div/span/div/div[2]/button")).click();
@@ -321,10 +305,6 @@ public class StatusTest {
         // Select back button
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[2]/div[1]/span/div/span/div/header/div/div[1]/div")).click();
         Thread.sleep(2000);
-
-
-
-
     }
 
     @AfterClass
@@ -333,3 +313,5 @@ public class StatusTest {
     }
 
 }
+
+
