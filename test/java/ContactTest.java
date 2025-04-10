174149950600aab3913eec982292f22ed75eacdc
Deividas Ilgunas <131389233+dilgunas20033@users.noreply.github.com>
2025-04-09 23:40:36 -0400

    Update ContactTest.java
    Added people to favorites and unadding them method

--- test/java/ContactTest.java
+++ test/java/ContactTest.java
@@ -28,15 +28,50 @@ public class CommunityTest {
         robot = new Robot();
     }
 
-    @Test(priority = 1) public void downloadPhotoandDelete() throws InterruptedException {
+    @Test(priority = 1) public void addingContactToFavorite() throws InterruptedException {
 
         driver.get("https://web.whatsapp.com/");
 
+        // Opens Whatsapp
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div/div[3]/div/header/div/div/div/div/span/div/div[2]/div[2]/button")));
         Thread.sleep(2000);
 
+        // Getting to contacts page
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[3]/div/div[3]/div[1]/div/div/div[1]/div/div/div")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[4]/div/header/div[3]/div/div[3]/div/button/span")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/span[5]/div/ul/div/div/li[1]/div")).click();
+        Thread.sleep(4000);
 
+        // Scroll down
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 1000);");
+
+        // Select favorite
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[5]/span/div/span/div/div/section/div[7]/div/div[2]")).click();
+        Thread.sleep(4000);
+
+        // Show favorites
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[3]/div/div[2]/button[3]")).click();
+        Thread.sleep(4000);
+
+        // Goes Back
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[3]/div/div[2]/button[1]")).click();
+        Thread.sleep(5000);
+
+        // Unfavorites
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[5]/span/div/span/div/div/section/div[7]/div/div[2]")).click();
+        Thread.sleep(4000);
+
+        // Show favorites
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[3]/div/div[2]/button[3]")).click();
+        Thread.sleep(4000);
+
+        // Goes Back
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[3]/div/div[2]/button[1]")).click();
+        Thread.sleep(5000);
     }
 
     @Test(priority = 2) public void addingToFavorites() throws InterruptedException {
